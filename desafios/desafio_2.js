735875f26b6746abb6a21c2dfbcd5a3866ac5df9
alexandreValenzuela <richard.valenzuela.et7@gmail.com>
2025-06-01 19:33:14 -0300

    realize el desafio 2 de talento tech y dividi las secciones del html de la pagina

---
+++ desafios/desafio_2.js
@@ -0,0 +1,5 @@
+const nombreProducto="mini baggio";
+let precioUnitario=700;
+let cantidadDeseada=prompt("Cuantos "+nombreProducto+" desea comprar");
+let preciofinal=precioUnitario*cantidadDeseada;
+alert("el precio final de "+cantidadDeseada+" "+nombreProducto+" es de "+preciofinal+" pesos")
