d672d9342b8a79ff181cc67dfaedc5d97247580c
alexandreValenzuela <richard.valenzuela.et7@gmail.com>
2025-06-02 19:43:15 -0300

    realice el desafio 5 y estableci la paleta de colores y la base del footer y el header de mi proyecto

---
+++ desafios/desafio_5.js
@@ -0,0 +1,20 @@
+const electrodomesticos = ["microondas", "tostadora", "pava electrica", "licuadora", "cafetera"];
+
+console.log("Lista original:");
+for (let i = 0; i < electrodomesticos.length; i++) {
+  console.log(electrodomesticos[i]);
+}
+
+console.log("\nLista actualizada:");
+
+let ventaProductos = 2;
+
+for (let i = 0; i < ventaProductos; i++) {
+  const vendido = electrodomesticos.pop();
+  
+}
+
+
+for (let i = 0; i < electrodomesticos.length; i++) {
+  console.log(electrodomesticos[i]);
+}
