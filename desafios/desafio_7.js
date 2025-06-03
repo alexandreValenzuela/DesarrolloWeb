0e7816d1a57d819c6e46fdffc8b36f1a1f429151
alexandreValenzuela <richard.valenzuela.et7@gmail.com>
2025-06-02 21:21:40 -0300

    realice el desafio 7 y cree un nuevo html llamado dom para realizarlo

---
+++ desafios/desafio_7.js
@@ -0,0 +1,8 @@
+    const nombre = document.getElementById("nombreProducto");
+    const precio = document.getElementById("precioProducto");
+
+    nombre.innerText = "Licuadora de alta potencia";
+    precio.innerText = "$450";
+
+    const titulo = document.querySelector("#nombreProducto");
+    titulo.style.color ="blue"; 
